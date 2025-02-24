c8fe059088840441afd55966cb8489cda4320b10
testbench4 <yashsingh1125@gmail.com>
2025-02-24 14:09:17 +0530

    Ttile,location  logic done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,8 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
  export const AppContext = createContext()
  export const AppContextProvider = (props)=>{
+   const [searchFilter, setSearchFilter] = useState({
+      title: '',
+      location: '',
+
+   })
+   const [isSearched,setIsSearched] = useState(false)
     const value = {
+      setSearchFilter,searchFilter,isSearched,setIsSearched
+
 
     }
      return (<AppContext.Provider value={value}>
