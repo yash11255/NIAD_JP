2fa6bc07e55468bd3b90775c99aa420b989a21dc
Siddhant Bansal <siddhant6338bansal@gmail.com>
2025-03-01 21:53:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
-import React, { useContext } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Application from './pages/Application';
-import ApplyJob from './pages/ApplyJob';
-import RecruiterLogin from './Components/RecruiterLogin';
-import { AppContext } from './context/AppContext';
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Application from "./pages/Application";
+import ApplyJob from "./pages/ApplyJob";
+import RecruiterLogin from "./Components/RecruiterLogin";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
   const { showRecruiterLogin, setShowRecruiterLogin } = useContext(AppContext);
+  // console.log("fro app", showRecruiterLogin);
   return (
     <div>
       {/* Recruiter Login Popup should only render when needed */}
       {showRecruiterLogin && (
-        <RecruiterLogin isOpen={showRecruiterLogin} onClose={() => setShowRecruiterLogin(false)} />
+        <RecruiterLogin
+          isOpen={showRecruiterLogin}
+          onClose={() => setShowRecruiterLogin(false)}
+        />
       )}
 
       {/* Page Routes */}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/application' element={<Application />} />
-        <Route path='/apply-job/:id' element={<ApplyJob />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
       </Routes>
     </div>
   );
