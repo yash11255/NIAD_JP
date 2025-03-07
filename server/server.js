60f7b819f9635e1fee1677f727a7d501596bad2a
yash Singh <69538056+yash11255@users.noreply.github.com>
2025-03-07 14:49:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bito-jobs-client.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
